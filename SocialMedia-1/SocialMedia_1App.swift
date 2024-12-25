fc563a6915dd3fd02dd935343db7947120bb4805
sahinkarahan <sahin.karahan@hotmail.com>
2024-12-25 14:36:39 +0300

    Initial Commit

---
+++ SocialMedia-1/SocialMedia_1App.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMedia_1App.swift
+//  SocialMedia-1
+//
+//  Created by Şahin Karahan on 25.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct SocialMedia_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
